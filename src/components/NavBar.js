86054f8ea9a9f84d734f256e7048ac558262dac3
park-d <54589582+park-d@users.noreply.github.com>
2022-11-01 14:47:35 -0500

    had help from instructor to fix hamburger

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,41 +2,46 @@ import React from 'react';
 
 function NavTabs({currentPage, handlePageChange}) {
     return (
-        <header >
-            <div className='d-flex justify-content-between container'>
-                <h1 className="py-5">DEVAN N. PARKISON <span>(park-d)</span></h1>
-                <nav className='d-flex align-items-center'>
+        <nav className="navbar navbar-expand-lg">
+            <div className = 'container'>
+            <h1 className="py-5">DEVAN N. PARKISON <span>(park-d)</span></h1>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
                     <a
                         href="#about"
                         onClick={() => handlePageChange('About')}
-                        className={currentPage === 'About' ? ' px-2  active' : 'px-2'}
+                        className={currentPage === 'About' ? 'activeCustom px-2' : 'px-2'}
                     >
                         About
                     </a>
                     <a
                         href="#portfolio"
                         onClick={() => handlePageChange('Portfolio')}
-                        className={currentPage === 'Portfolio' ? ' px-2  active' : 'px-2'}
+                        className={currentPage === 'Portfolio' ? 'activeCustom px-2' : 'px-2'}
                     >
                         Portfolio
                     </a>
                     <a
                         href="#contact"
                         onClick={() => handlePageChange('Contact')}
-                        className={currentPage === 'Contact' ? ' px-2  active' : 'px-2'}
+                        className={currentPage === 'Contact' ? 'activeCustom px-2' : 'px-2'}
                     >
                         Contact
                     </a>
                     <a
                         href="#resume"
                         onClick={() => handlePageChange('Resume')}
-                        className={currentPage === 'Resume' ? ' px-2  active' : 'px-2'}
+                        className={currentPage === 'Resume' ? 'activeCustom px-2' : 'px-2'}
                     >
                         Resume
                     </a>
-                </nav>
+                </div>
+                </div>
             </div>
-        </header>
+        </nav>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import '../src/styles/header.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
